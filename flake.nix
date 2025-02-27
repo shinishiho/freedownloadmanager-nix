15a4f700a97f9332c7b03df39d05d4d8169f2283
Hiten Tandon <tandonhiten@gamil.com>
2025-02-27 17:07:05 +0530

    Feat: Added FDM

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "gihub:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -14,21 +14,48 @@
       
         nativeBuildInputs = [
           dpkg
+          libxkbcommon
+          libGL
+          mysql80
+          xorg.libX11
+          xorg.libxcb
+          egl-wayland
+          libmysqlclient
+          gst_all_1.gst-devtools
+          gtk3
+          libpq
+          xorg.xcbutil
+          xcb-util-cursor
+          libpulseaudio
+          unixODBC
+          cairo
+          pango
+          autoPatchelfHook
+          kdePackages.wrapQtAppsHook
+          qt6.qtbase
+          wrapGAppsHook
         ];
 
-        phases = ["unpackPhase" "installPhase" "fixupPhase"];
+        
+        runtimeDependencies = [
+          (lib.getLib udev)
+        ];
 
-        postUnpackPhase = ''
-          dpkg-deb -x $src $out
-          '';
 
         installPhase = ''
-          '';
-      
+          export tmp=$(mktemp -d)
+          dpkg-deb -x $src $tmp
+          mkdir $out
+          cp -r $tmp/opt/freedownloadmanager/* $out
+          patchelf $out/plugins/sqldrivers/libqsqlmimer.so --remove-needed libmimerapi.so
+          cp -r $tmp/usr/share $out
+        '';
+
         meta = {
           description = "Free Download Manager";
           homepage = "https://www.freedownloadmanager.org";
         };
       });
+      fomatter = nixfmt-classic;
   });
 }
