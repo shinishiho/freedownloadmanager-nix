e94d40bb0e9f07e0ba943c97291bc19492b14c8c
Hiten Tandon <tandonhiten@gamil.com>
2025-02-27 18:44:20 +0530

    Fix: Flake now reflects that it only works for x86_64-linux

--- flake.nix
+++ flake.nix
@@ -4,62 +4,66 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem(system: 
-    with import nixpkgs {inherit system;}; {
-      packages.default = stdenv.mkDerivation (finalAttrs: {
-        pname = "freedownloadmanager";
-        version = "6.25.2";
-      
-        src = builtins.fetchurl "https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb";
-      
-        nativeBuildInputs = [
-          dpkg
-          libxkbcommon
-          libGL
-          mysql80
-          xorg.libX11
-          xorg.libxcb
-          egl-wayland
-          libmysqlclient
-          gst_all_1.gst-devtools
-          gtk3
-          libpq
-          xorg.xcbutil
-          xcb-util-cursor
-          libpulseaudio
-          unixODBC
-          cairo
-          pango
-          autoPatchelfHook
-          kdePackages.wrapQtAppsHook
-          qt6.qtbase
-          wrapGAppsHook
-        ];
+  outputs = { nixpkgs, flake-utils, ... }:
+    #TODO: Add support for more systems.
+    flake-utils.lib.eachSystem (["x86_64-linux"]) (system:
+      with import nixpkgs { inherit system; }; {
+        packages.default = stdenv.mkDerivation (finalAttrs: {
+          pname = "freedownloadmanager";
+          version = "6.25.2";
 
-        
-        runtimeDependencies = [
-          (lib.getLib udev)
-        ];
+          src = builtins.fetchurl {
+            url =
+              "https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb";
+            sha256 = "1ypkcpl0lkixbgbf68081njfavys2mi6sndmvsvxbff7a7g15a8v";
+          };
 
+          nativeBuildInputs = [
+            dpkg
+            libxkbcommon
+            libGL
+            mysql80
+            xorg.libX11
+            xorg.libxcb
+            egl-wayland
+            libmysqlclient
+            gst_all_1.gst-devtools
+            gtk3
+            libpq
+            xorg.xcbutil
+            xcb-util-cursor
+            libpulseaudio
+            unixODBC
+            cairo
+            pango
+            autoPatchelfHook
+            kdePackages.wrapQtAppsHook
+            qt6.qtbase
+            wrapGAppsHook
+          ];
 
-        installPhase = ''
-          export tmp=$(mktemp -d)
-          dpkg-deb -x $src $tmp
-          mkdir $out
-          cp -r $tmp/opt/freedownloadmanager/* $out
-          patchelf $out/plugins/sqldrivers/libqsqlmimer.so --remove-needed libmimerapi.so
-          cp -r $tmp/usr/share $out
-          mkdir $out/bin
-          ln -sf $out/fdm $out/bin/freedownloadmanager
-          substituteInPlace $out/share/applications/freedownloadmanager.desktop --replace "Exec=/opt/freedownloadmanager/fdm" "Exec=freedownloadmanager" --replace "Icon=/opt/freedownloadmanager/icon.png" "Icon=$out/freedownloadmanager/icon.png"
-        '';
+          runtimeDependencies = [ (lib.getLib udev) ];
 
-        meta = {
-          description = "Free Download Manager";
-          licenses = lib.licenses.unfree;
-          homepage = "https://www.freedownloadmanager.org";
-        };
+          installPhase = ''
+            export tmp=$(mktemp -d)
+            dpkg-deb -x $src $tmp
+            mkdir $out
+            cp -r $tmp/opt/freedownloadmanager/* $out
+            patchelf $out/plugins/sqldrivers/libqsqlmimer.so --remove-needed libmimerapi.so
+            cp -r $tmp/usr/share $out
+            mkdir $out/bin
+            ln -sf $out/fdm $out/bin/freedownloadmanager
+            substituteInPlace $out/share/applications/freedownloadmanager.desktop\
+              --replace "Exec=/opt/freedownloadmanager/fdm" "Exec=freedownloadmanager"\
+              --replace "Icon=/opt/freedownloadmanager/icon.png" "Icon=$out/freedownloadmanager/icon.png"
+          '';
+
+          meta = {
+            description = "Free Download Manager";
+            licenses = lib.licenses.unfree;
+            homepage = "https://www.freedownloadmanager.org";
+          };
+        });
+        formatter = nixfmt-classic;
       });
-      fomatter = nixfmt-classic;
-  });
 }
